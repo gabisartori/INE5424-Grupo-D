bf16a1bd986baa90cb22f111ee4cdd4832acc030
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2024-09-12 20:36:37 -0300

    correções menores

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,11 @@
-pub const P1_ADDR: &str = "127.0.0.1:3000";
-pub const P2_ADDR: &str = "127.0.0.1:3001";
-pub const P3_ADDR: &str = "127.0.0.1:3002";
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+
+// Endereço IP do localhost
+pub const LOCALHOST: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
+pub const P1_ADDR: SocketAddr = SocketAddr::new(LOCALHOST, 3000);
+pub const P2_ADDR: SocketAddr = SocketAddr::new(LOCALHOST, 3001);
+pub const P3_ADDR: SocketAddr = SocketAddr::new(LOCALHOST, 3002);
 
 pub const TIMEOUT: u64 = 1000; // 1 segundo
 pub const HEARTBEAT_INTERVAL: u64 = 500; // 500 milissegundos
@@ -8,4 +13,4 @@ pub const FAILURE_DETECTION_INTERVAL: u64 = 1000; // 1 segundo
 pub const BUFFER_SIZE: usize = 1024;
 
 // um vetor com os endereços dos processos
-// pub const NODES: [&str; 2] = [P1_ADDR, P2_ADDR, P3_ADDR];
+pub const NODES: [SocketAddr; 3] = [P1_ADDR, P2_ADDR, P3_ADDR];
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,23 @@
 #![allow(unused_variables)]
 #![allow(unused_mut)]
 #![allow(dead_code)]
-mod lib {
-    pub mod reliable_communication;
-    pub mod channels;
-    pub mod failure_detection;
-}
-use config::{BUFFER_SIZE, P1_ADDR, P2_ADDR, P3_ADDR, TIMEOUT, HEARTBEAT_INTERVAL, FAILURE_DETECTION_INTERVAL};
-use lib::reliable_communication::ReliableCommunication;
-
-// Importa as configurações de endereços dos processos
-mod config;
 
 use std::net::SocketAddr;
 use std::thread;
 use std::sync::Arc;
 use std::env;
 use rand::Rng;
+
+mod lib {
+    pub mod reliable_communication;
+    pub mod channels;
+    pub mod failure_detection;
+}
+use lib::reliable_communication::ReliableCommunication;
+
+// Importa as configurações de endereços dos processos
+mod config;
+use config::{BUFFER_SIZE, NODES, TIMEOUT, HEARTBEAT_INTERVAL, FAILURE_DETECTION_INTERVAL};
 
 struct Agent {
     addr: SocketAddr,
@@ -39,26 +40,28 @@ impl Agent {
         loop {
             let mut message: [u8; 1024] = [0; 1024];
             let (size, sender) = self.communication.receive(&mut message);
-            println!("\nAgent {} receiving {} bytes from {}\nMessage: {}\n", self.agent_number, size, sender, std::str::from_utf8(&message).unwrap());
+            println!("\nAgent {} receiving {} bytes from {}\nMessage: {}\n",
+                self.agent_number, size, sender, std::str::from_utf8(&message).unwrap());
         }
     }
 
     fn sender(&self, user_controlled: bool) {
         println!("Agent {} is sending messages", self.agent_number);
-        let group = vec![P1_ADDR, P2_ADDR, P3_ADDR];
 
-        for p in group {
-            let address: SocketAddr = p.parse().unwrap();
-            println!("\nAgent {} sending message to {}\n", self.agent_number, address);
-            let msg = format!("Hello agent {}", address);
-
-            let message = msg.as_bytes();
-            // format the message to a [u8; 1024] size
-            let mut message_array: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
-            for i in 0..message.len() {
-                message_array[i] = message[i];
-            }
-            self.communication.send(&address, &message_array);
+        for addr in NODES {
+            println!("\nAgent {} sending message to {}\n", self.agent_number, addr);
+            
+            let message: [u8; 1024] = {
+                let msg = format!("Hello agent {}", addr);
+                let msg = msg.as_bytes();
+                // format the message to a [u8; 1024] size
+                let mut message_array: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+                for i in 0..msg.len() {
+                    message_array[i] = msg[i];
+                }
+                message_array
+            };
+            self.communication.send(&addr, &message);
             thread::sleep(std::time::Duration::from_secs(rand::thread_rng().gen_range(1..10)));
         }
     }
@@ -77,21 +80,20 @@ impl Agent {
 
 
 fn main() {
-    let group: Vec<SocketAddr> = vec![P1_ADDR, P2_ADDR, P3_ADDR].iter().map(|p| p.parse().unwrap()).collect();
     let args: Vec<String> = env::args().collect();
     let mut agent_number = match args.len() {
         2 => args[1].parse::<u32>().unwrap(),
         _ => 4,
     };
 
-    if agent_number > group.len() as u32 {
-        agent_number = group.len() as u32;
+    if agent_number > NODES.len() as u32 {
+        agent_number = NODES.len() as u32;
     }
 
     let mut agent_handlers: Vec<thread::JoinHandle<()>> = Vec::new();
 
     for i in 0..agent_number {
-        let address: SocketAddr  = group[i as usize];
+        let address: SocketAddr  = NODES[i as usize];
         let agent: Arc<Agent> = Arc::new(Agent::new(address, i));
         agent_handlers.push(thread::spawn(move || agent.run()));
     }
