38dfd19ccc9241849cc45676860ddaa19ed35b4c
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-09-12 04:00:49 -0300

    Add functionality to send and receive through udp sockets and also implement rudimentar agent behavior for send and receive testing

--- src/lib/reliable_communication.rs
+++ src/lib/reliable_communication.rs
@@ -1,15 +1,33 @@
-pub fn send(message: &str) {
-    println!("Sending message: {}", message);
+use std::net::UdpSocket;
+
+pub struct ReliableCommunication {
+    socket: UdpSocket,
 }
 
-pub fn receive() -> &'static str {
-    "Hello, world!"
-}
+impl ReliableCommunication {
+    pub fn new(port: u16) -> ReliableCommunication {
+        println!("Creating new ReliableCommunication instance");
+        let socket = UdpSocket::bind(format!("127.0.0.1:{}", port)).unwrap();
+        println!("Socket bound to {}", socket.local_addr().unwrap());
+        ReliableCommunication {
+            socket: socket,
+        }
+    }
 
-pub fn broadcast(message: &str) {
-    println!("Broadcasting message: {}", message);
-}
 
-pub fn deliver(message: &str) {
-    println!("Delivering message: {}", message);
+    pub fn send(&self, host: &str, port: u16, message: &str) {
+        self.socket.send_to(message.as_bytes(), format!("{}:{}", host, port)).unwrap();
+    }
+
+    pub fn receive(&self, message: &mut [u8]) -> (usize, std::net::SocketAddr) {
+        self.socket.recv_from(message).unwrap()
+    }
+
+    pub fn broadcast(message: &str) {
+        println!("Broadcasting message: {}", message);
+    }
+
+    pub fn deliver(message: &str) {
+        println!("Delivering message: {}", message);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ mod lib {pub mod reliable_communication;}
 use std::thread;
 use std::sync::Arc;
 use std::env;
+use rand::Rng;
+
+use lib::reliable_communication;
 
 struct Agent {
     port: u16,
     agent_number: u32,
+    communication: reliable_communication::ReliableCommunication
 }
 
 impl Agent {
@@ -14,17 +18,31 @@ impl Agent {
         Agent {
             port,
             agent_number,
+            communication: reliable_communication::ReliableCommunication::new(port)
         }
     }
 
     fn listener(&self) {
         println!("Agent {} is listening", self.agent_number);
-
+        loop {
+            let mut message = [0; 1024];
+            let (size, sender) = self.communication.receive(&mut message);
+            println!("\nAgent {} receiving {} bytes from {}\nMessage: {}\n", self.agent_number, size, sender, std::str::from_utf8(&message).unwrap());
+        }
     }
 
     fn sender(&self, user_controlled: bool) {
         println!("Agent {} is sending messages", self.agent_number);
 
+        loop {
+            let n = rand::thread_rng().gen_range(0..4);
+            let host = "127.0.0.1";
+            let port = 3000 + n;
+            println!("\nAgent {} sending message to {}:{}\n", self.agent_number, host, port);
+            let message = format!("Hello agent {}", n);
+            self.communication.send(host, port, &message);
+            thread::sleep(std::time::Duration::from_secs(rand::thread_rng().gen_range(1..10)));
+        }
     }
 
     fn run(self: Arc<Self>) {
@@ -54,6 +72,8 @@ fn main() {
         let agent = Arc::new(Agent::new(start_port + (i as u16), i));
         agent_handlers.push(thread::spawn(move || agent.run()));
     }
+
+    println!();
 
     for handler in agent_handlers {
         handler.join().unwrap();
