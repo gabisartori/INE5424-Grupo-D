36d0e6fa202910bdbee3f8c077d6936f46e72da5
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2024-09-12 19:39:41 -0300

    merge conexão-udp e Maykon

---
+++ src/lib/channels.rs
@@ -0,0 +1,37 @@
+/*
+A camada de comunicação mais baixa, representa os canais de comunicação (channels)
+e implementa sockets para comunicação entre os processos participantes.
+*/
+
+use crate::config::BUFFER_SIZE;
+// use super::failure_detection;
+
+// Importações necessárias
+use std::net::{UdpSocket, SocketAddr};
+use std::io::Error;
+
+// Estrutura básica para a camada de comunicação por canais
+pub struct Channel {
+    socket: UdpSocket,
+}
+
+impl Channel {
+    // Função para criar um novo canal
+    pub fn initialize(bind_addr: &SocketAddr) -> Result<Self, Error> {
+        let socket = UdpSocket::bind(bind_addr)?;
+        // socket.set_nonblocking(true)?;
+        Ok(Self { socket: socket})
+    }
+
+    // Função para enviar mensagem para um destinatário específico
+    pub fn send(&self, dst_addr: &SocketAddr, message: &[u8; BUFFER_SIZE]) -> Result<(), Error> {
+        self.socket.send_to(message, dst_addr)?;
+        Ok(())
+    }
+
+    // Função para receber mensagens
+    pub fn receive(&self, buffer: &mut [u8]) -> Result<(usize, SocketAddr), Error> {
+        let (size, src) = self.socket.recv_from(buffer)?;
+        Ok((size, src))
+    }
+}
---
+++ src/lib/failure_detection.rs
@@ -0,0 +1,6 @@
+/*
+A camada de detecção de defeitos (Failure Detection) permite que processos
+    participantes na comunicação monitorem uns aos outros
+e sinalizem os protocolos de comunicação confiável sobre possíveis saídas de processos do grupo
+(intencionais ou não-intencionais, no caso de falhas).
+*/
