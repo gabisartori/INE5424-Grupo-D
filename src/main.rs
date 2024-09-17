200dfc3f14df01fcc682c7ed931fecc89cc5e348
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2024-09-17 16:48:29 -0300

    Adaptação para o uso de vetores

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use lib::reliable_communication::ReliableCommunication;
 
 // Importa as configurações de endereços dos processos
 mod config;
-use config::{Node, LOCALHOST, NODES};
+use config::{Node, BUFFER_SIZE, LOCALHOST, NODES};
 
 struct Agent {
     id: u32,
@@ -37,11 +37,26 @@ impl Agent {
 
     fn listener(&self) {
         // println!("Agent {} is listening", self.id);
-        loop {
-            let mut message: [u8; 1024] = [0; 1024];
+        // loop
+        {
+            let mut message: Vec<u8> = Vec::new();
             let (size, sender) = self.communication.receive(&mut message);
-            println!("\nAgent {} receiving {} bytes from {}\nMessage: {}\n",
-                self.id, size, sender, std::str::from_utf8(&message).unwrap());
+            // println!("\nAgent {} receiving {} bytes from {}\nMessage: {}\n", self.id, size, sender, std::str::from_utf8(&message).unwrap());
+            print!("Agent {} receiving {} bytes from {}\nMessage: ", self.id, size, sender);
+            // divide a mensagem em chunks de forma que possa ser convertida para string
+            let chuncked_msg = message.chunks(BUFFER_SIZE);
+            let mut full_msg: Vec<&str> = Vec::new();
+            for m in chuncked_msg {
+                let msg: Result<&str, std::str::Utf8Error> = std::str::from_utf8(&m);
+                match msg {
+                    Ok(msg) => {
+                        print!("{}", msg);
+                        full_msg.push(msg); },
+                    Err(e) => { println!("\n-------------------------\nError\n------------------");                   
+                    }
+                }
+            }
+            // let msg = full_msg.join("");
         }
     }
 
@@ -50,7 +65,8 @@ impl Agent {
 
         // Choice of destination for each message the agent sends
         let mut destination: u32;
-        loop {
+        // loop
+        {
             // Pick a random node to send a message to
             // If the random node is the agent itself, pick another one
             loop {
@@ -66,9 +82,10 @@ impl Agent {
                 =>=> Porque format! retorna um String e não um &str,
                 => se não for alocado na memória (em uma variável), não dá pra passar a referência (&[u8])
             */
-            let msg: String = format!("Hello from agent {}", self.id);
-            // let msg: String = config::LARGE_MSG.to_string();
-            let msg: &[u8] = msg.as_bytes();
+            // let msg: String = format!("Hello from agent {}", self.id);
+            let msg: String = config::LARGE_MSG.to_string();
+            println!("the message has {} bytes and is:\n{}", msg.len(), msg);
+            let msg: Vec<u8> = msg.as_bytes().to_vec();
             println!("Agent {} sending message to agent {}", self.id, destination);
             self.communication.send(&(self.communication.group[destination as usize].addr), msg);
 
