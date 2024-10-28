65907e0d740b60a8cd5c9346efb248d78b38206b
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-10-28 17:49:21 -0300

    Add new tests for atomic broadcast

--- src/tests.rs
+++ src/tests.rs
@@ -15,9 +15,9 @@ pub enum Action {
     }
 }
 
-const MSG: &str = "
+const MSG_0: &str = "
 --------------------------------------
-START OF MESSAGE
+START OF MESSAGE 0
 --------------------------------------
 Universidade Federal de Santa Catarina - UFSC
 Departamento em Informática e Estatística
@@ -47,6 +47,69 @@ END OF MESSAGE
 --------------------
 ";
 
+const MSG_1: &str = "
+--------------------------------------
+START OF MESSAGE 1
+--------------------------------------
+Universidade Federal de Santa Catarina - UFSC
+Departamento em Informática e Estatística
+INE5424-06208B - Sistemas Operacionais II
+Semestre 2024/2
+Projeto e Implementação de uma Biblioteca para Comunicação Confiável entre Processos
+1. Objetivos e Escopo
+Este projeto consiste em desenvolver uma biblioteca de comunicação capaz de garantir a
+entrega confiável de mensagens entre os processos participantes de um grupo. Dessa
+forma, programas que utilizem a biblioteca irão usufruir de garantias na entrega de
+mensagens, como entrega confiável, difusão com entrega para todos os processos corretos
+ou nenhum, ou, ainda, garantias de ordem na entrega, como ordenação FIFO e total.
+A biblioteca deverá disponibilizar para o usuário as primitivas send(id,m) e receive(m)
+para mensagens destinadas a um processo específico (comunicação 1:1), onde id é o
+identificador do destinatário e m é uma mensagem; e primitivas broadcast(m) e
+deliver(m) para mensagens destinadas a todos os participantes (comunicação 1:n),
+sendo m uma mensagem e n o número total de participantes.
+O desafio está em preservar propriedades de entrega confiável em ambientes não
+confiáveis, onde processos podem falhar e mensagens podem ser perdidas quando
+transmitidas pelos protocolos de rede subjacentes. A biblioteca deve ser implementada na
+linguagem C++ e a comunicação entre processos deve ser feita por sockets padrão
+(Berkeley sockets / POSIX.1-2008). Não é admitido o uso de bibliotecas pré-existentes para
+comunicação confiável ou que apresentem outras abstrações de comunicação para além do
+uso de sockets padrão.
+--------------------
+END OF MESSAGE
+--------------------
+";
+
+const MSG_2: &str = "
+--------------------------------------
+START OF MESSAGE 2
+--------------------------------------
+Universidade Federal de Santa Catarina - UFSC
+Departamento em Informática e Estatística
+INE5424-06208B - Sistemas Operacionais II
+Semestre 2024/2
+Projeto e Implementação de uma Biblioteca para Comunicação Confiável entre Processos
+1. Objetivos e Escopo
+Este projeto consiste em desenvolver uma biblioteca de comunicação capaz de garantir a
+entrega confiável de mensagens entre os processos participantes de um grupo. Dessa
+forma, programas que utilizem a biblioteca irão usufruir de garantias na entrega de
+mensagens, como entrega confiável, difusão com entrega para todos os processos corretos
+ou nenhum, ou, ainda, garantias de ordem na entrega, como ordenação FIFO e total.
+A biblioteca deverá disponibilizar para o usuário as primitivas send(id,m) e receive(m)
+para mensagens destinadas a um processo específico (comunicação 1:1), onde id é o
+identificador do destinatário e m é uma mensagem; e primitivas broadcast(m) e
+deliver(m) para mensagens destinadas a todos os participantes (comunicação 1:n),
+sendo m uma mensagem e n o número total de participantes.
+O desafio está em preservar propriedades de entrega confiável em ambientes não
+confiáveis, onde processos podem falhar e mensagens podem ser perdidas quando
+transmitidas pelos protocolos de rede subjacentes. A biblioteca deve ser implementada na
+linguagem C++ e a comunicação entre processos deve ser feita por sockets padrão
+(Berkeley sockets / POSIX.1-2008). Não é admitido o uso de bibliotecas pré-existentes para
+comunicação confiável ou que apresentem outras abstrações de comunicação para além do
+uso de sockets padrão.
+--------------------
+END OF MESSAGE
+--------------------
+";
 
 type Test = Vec<Vec<Action>>;
 
@@ -81,7 +144,7 @@ pub fn send_test_2() -> Test {
         ],
         // Agent 1
         vec![
-            Action::Send { destination: 2, message: MSG.to_string() },
+            Action::Send { destination: 2, message: MSG_0.to_string() },
             Action::Receive {
                 message: "message_0".to_string(),
             }
@@ -89,7 +152,7 @@ pub fn send_test_2() -> Test {
         // Agent 2
         vec![
             Action::Receive {
-                message: MSG.to_string(),
+                message: MSG_0.to_string(),
             }
         ]
     ] 
@@ -126,20 +189,20 @@ pub fn broadcast_test_2() -> Test {
         // Agent 0
         vec![
             Action::Broadcast {
-                message: MSG.to_string(),
+                message: MSG_0.to_string(),
             },
-            Action::Receive { message: MSG.to_string() },
+            Action::Receive { message: MSG_0.to_string() },
         ],
         // Agent 1
         vec![
             Action::Receive {
-                message: MSG.to_string(),
+                message: MSG_0.to_string(),
             }
         ],
         // Agent 2
         vec![
             Action::Receive {
-                message: MSG.to_string(),
+                message: MSG_0.to_string(),
             }
         ]
     ]
@@ -178,6 +241,43 @@ pub fn broadcast_test_3() -> Test {
             Action::Receive { message: "Mensagem 1".to_string() },
             Action::Broadcast { message: "Mensagem 2".to_string() },
             Action::Receive { message: "Mensagem 2".to_string() },
+        ]
+    ]
+}
+
+#[allow(dead_code)]
+pub fn broadcast_test_4() -> Test {
+    vec![
+        // Agent 0
+        vec![
+            Action::Receive { message: MSG_0.to_string() },
+            Action::Die { after_n_messages: 1 },
+            Action::Receive { message: MSG_1.to_string() },
+            Action::Receive { message: MSG_2.to_string() },
+
+        ],
+        // Agent 1
+        vec![
+            Action::Receive { message: MSG_0.to_string() },
+            Action::Receive { message: MSG_1.to_string() },
+            Action::Receive { message: MSG_2.to_string() },
+        ],
+        // Agent 2
+        vec![
+            Action::Broadcast { message: MSG_0.to_string() },
+            Action::Broadcast { message: MSG_1.to_string() },
+
+            Action::Receive { message: MSG_0.to_string() },
+            Action::Receive { message: MSG_1.to_string() },
+            Action::Receive { message: MSG_2.to_string() },
+
+        ],
+        // Agent 3
+        vec![
+            Action::Receive { message: MSG_0.to_string() },
+            Action::Receive { message: MSG_1.to_string() },
+            Action::Broadcast { message: MSG_2.to_string() },
+            Action::Receive { message: MSG_2.to_string() },
         ]
     ]
 }
