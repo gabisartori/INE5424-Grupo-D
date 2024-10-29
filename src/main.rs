0edb1f626814a7167021ac657ef79000e856d27a
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-10-29 04:25:34 -0300

    Organize main functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::{IpAddr, SocketAddr};
-use std::sync::mpsc::{RecvError, Sender};
+use std::sync::mpsc::{RecvError, Sender, self};
 use std::sync::{Arc, Mutex};
 use std::thread;
 // use rand::Rng;
@@ -22,8 +22,6 @@ impl Agent {
     fn new(
         id: usize,
         nodes: Vec<Node>,
-        // TODO: Update agent struct to match new test format
-        _n_msgs: u32,
         logger: SharedLogger,
     ) -> Result<Self, std::io::Error> {
         Ok(Agent {
@@ -37,6 +35,83 @@ impl Agent {
         })
     }
 
+    /// Start both creater and receiver threads
+    /// Also runs some logic for if any of the threads is supposed to trigger the death of the entire agent in the test
+    pub fn run(self: Arc<Self>, actions: Vec<Action>) {
+        // Builds the instruction vectors
+        let mut send_actions = Vec::new();
+        let mut receive_actions = Vec::new();
+        for action in actions {
+            match action {
+                Action::Send(action) => {
+                    send_actions.push(action);
+                },
+                Action::Receive(action) => {
+                    receive_actions.push(action);
+                },
+                // Die instruction means the agent shouldn't even start running
+                Action::Die() => {
+                    let path = format!("tests/Resultado.txt");
+                    let msg = format!(
+                        "AGENTE {} -> ENVIOS: 0 - RECEBIDOS: 0\n",
+                        self.id
+                    );
+                    debug_file!(path, &msg.as_bytes());
+                    return;
+                }
+            }
+        }
+    
+        // Channel for handling the death instruction
+        let (death_tx, death_rx) = mpsc::channel();
+        let death_tx_clone = death_tx.clone();
+        
+        // Spawns both threads
+        let sender_clone = Arc::clone(&self);
+        let listener_clone = Arc::clone(&self);
+        
+        let sender = thread::spawn(move || sender_clone.creater(send_actions, death_tx_clone));
+        let listener = thread::spawn(move || listener_clone.receiver(receive_actions, death_tx));
+        
+        // Threads results
+        let s_acertos;
+        let r_acertos;
+
+        // Waits for either any of the threads to send in the channel, meaning the agent should die
+        // If both threads end without sending anything, the agent should wait for them to finish
+        match death_rx.recv() {
+            Ok(n) => {
+                // TODO: Decidir o que fazer com o valor recebido, se foi enviado pela creater ou pela receiver
+                r_acertos = n;
+                s_acertos = n;
+            },
+            Err(RecvError) => {
+                r_acertos = match listener.join() {
+                    Ok(r) => r,
+                    Err(_) => {
+                        debug_println!("Agent {}: Erro ao esperar thread listener encerrar", self.id);
+                        0
+                    },
+                };
+                s_acertos = match sender.join() {
+                    Ok(s) => s,
+                    Err(_) => {
+                        debug_println!("Agent {}: Erro ao esperar thread sender encerrar", self.id);
+                        0
+                    },
+                };
+            }
+        }
+        
+        // Output the results to a file
+        let msg = format!(
+            "AGENTE {} -> ENVIOS: {s_acertos} - RECEBIDOS: {r_acertos}\n",
+            self.id
+        );
+        debug_file!("tests/Resultado.txt", &msg.as_bytes());
+    }
+
+    /// Agent thread that always receives messages and checks if they are the expected ones from the selected test
     fn receiver(&self, actions: Vec<ReceiveAction>, death_tx: Sender<u32>) -> u32 {
         let mut acertos = 0;
         let mut i = 0;
@@ -77,6 +152,7 @@ impl Agent {
         return acertos;
     }
 
+    /// Agent thread that sends preset messages from the selected test
     fn creater(&self, actions: Vec<SendAction>, death_tx: Sender<u32>) -> u32 {
         let mut acertos = 0;
         for action in actions {
@@ -96,102 +172,82 @@ impl Agent {
         }
         return acertos;
     }
+}
 
-    pub fn run(self: Arc<Self>, actions: Vec<Action>) {
-        let mut send_actions = Vec::new();
-        let mut receive_actions = Vec::new();
-        for action in actions {
-            match action {
-                Action::Send(action) => {
-                    send_actions.push(action);
-                },
-                Action::Receive(action) => {
-                    receive_actions.push(action);
-                },
-                Action::Die() => {
-                    let path = format!("tests/Resultado.txt");
-                    let msg = format!(
-                        "AGENTE {} -> ENVIOS: 0 - RECEBIDOS: 0\n",
-                        self.id
-                    );
-                    debug_file!(path, &msg.as_bytes());
-                    return;
-                }
-            }
-        }
-    
-        let sender_clone = Arc::clone(&self);
-        let listener_clone = Arc::clone(&self);
-        
-        // Cria threads para enviar e receber mensagens e recupera o retorno delas
-        let (death_tx, death_rx) = std::sync::mpsc::channel();
-        let death_tx_clone = death_tx.clone();
-        let sender = thread::spawn(move || sender_clone.creater(send_actions, death_tx_clone));
-        let listener = thread::spawn(move || listener_clone.receiver(receive_actions, death_tx));
-        
-        let s_acertos;
-        let r_acertos;
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut test = tests::broadcast_test_2();
+    let agent_num = test.len();
 
-        match death_rx.recv() {
-            Ok(n) => {
-                // listener recebeu instrução DIE, então a thread deve morrer
-                r_acertos = n;
-                s_acertos = 0;
-            },
-            Err(RecvError) => {
-                // listener encerrou sem receber instrução DIE, esperar sender encerrar também
-                r_acertos = match listener.join() {
-                    Ok(r) => r,
-                    Err(_) => {
-                        debug_println!("Agent {}: Erro ao esperar thread listener encerrar", self.id);
-                        0
-                    },
-                };
-                s_acertos = match sender.join() {
-                    Ok(s) => s,
-                    Err(_) => {
-                        debug_println!("Agent {}: Erro ao esperar thread sender encerrar", self.id);
-                        0
-                    },
-                };
-            }
+    if args.len() == 14 {
+        // Processo principal inicializando os agentes
+        let mut children = Vec::new();
+        for i in 0..agent_num {
+            let c = std::process::Command::new(std::env::current_exe().unwrap())
+                .args(args[1..].as_ref()) // Passando o número de agentes
+                .arg(i.to_string()) // Passando o ID do agente
+                .spawn()
+                .expect(format!("Falha ao spawnar processo {i}").as_str());
+            children.push(c);
         }
-        let path = format!("tests/Resultado.txt");
-        let msg = format!(
-            "AGENTE {} -> ENVIOS: {s_acertos} - RECEBIDOS: {r_acertos}\n",
-            self.id
+        // Aguardar a finalização de todos os agentes
+        for mut c in children {
+            c.wait().expect("Falha ao esperar processo filho");
+        }
+        calculate_test(agent_num);
+    } else if args.len() == 15 {
+        // Sub-processo: Execução do agente
+        let ip: IpAddr = args[8]
+            .parse()
+            .expect("Falha ao converter ip para IpAddr");
+        let port: u16 = args[9]
+            .parse()
+            .expect("Falha ao converter port para u16");
+        let agent_id: usize = args.last().unwrap()
+            .parse()
+            .expect("Falha ao converter agent_id para u32");
+        
+        let agent = create_agents(
+            agent_id,
+            agent_num,
+            ip,
+            port,
         );
-        debug_file!(path, &msg.as_bytes());
+        let actions = test.remove(agent_id);
+        match agent {
+            Ok(agent) => agent.run(actions),
+            Err(e) => panic!("Falha ao criar agente {}: {}", agent_id, e),
+        }
+    } else {
+        println!("uso: cargo run <agent_num> <n_msgs> <broadcast> <timeout> <timeout_limit> <message_timeout> <broadcast_timeout> <ip> <port> <gossip_rate> <w_size> <buffer_size> <loss_rate> <corruption_rate>");
+        println!("enviado {:?}", args);
+        panic!("Número de argumentos {} inválido", args.len());
     }
 }
 
+/// Creates the vector of nodes for all of the members of the group
+/// Since this is currently always being tested in the same machine, the IP is always the same and the ports are based on the node id
+/// Then it creates and returns the agent for the node that matches the sub-process id
 fn create_agents(
     id: usize,
     agent_num: usize,
-    n_msgs: u32,
     ip: IpAddr,
     port: u16,
 ) -> Result<Arc<Agent>, std::io::Error> {
-    let mut nodes: Vec<Node> = Vec::new();
-
-    // Contruir vetor unificando os nós locais e os remotos
-    for i in 0..agent_num {
-        nodes.push(Node::new(SocketAddr::new(ip, port + (i as u16)), i));
-    }
+    let nodes: Vec<Node> = (0..agent_num)
+        .map(|i| Node::new(SocketAddr::new(ip, port + (i as u16)), i))
+        .collect();
 
     let logger = Arc::new(Mutex::new(Logger::new(1, agent_num)));
 
     let agent = Arc::new(
-        Agent::new(
-            id, nodes,
-            n_msgs,
-            logger,        
-        )?
+        Agent::new(id, nodes, logger)?
     );
     Ok(agent)
 }
 
-// TODO: Fix this function so it works with the new logger
+// TODO: Comentar melhor essa função
+/// Reads and organizes the output of all sub-processes
 fn calculate_test(agent_num: usize) {
     let file = std::fs::File::open("tests/Resultado.txt").expect("Erro ao abrir o arquivo de log");
     let mut reader = std::io::BufReader::new(file);
@@ -231,60 +287,4 @@ fn calculate_test(agent_num: usize) {
 
     println!("Total de Mensagens Enviadas : {total_sends}");
     println!("Total de Mensagens Recebidas: {total_receivs}");
-}
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let mut test = tests::send_test_2();
-    let agent_num = test.len();
-
-    if args.len() == 14 {
-
-        let mut childs = Vec::new();
-
-        // Inicializar os agentes locais
-        for i in 0..agent_num {
-            let c = std::process::Command::new(std::env::current_exe().unwrap())
-                .args(args[1..].as_ref()) // Passando o número de agentes
-                .arg(i.to_string()) // Passando o ID do agente
-                .spawn()
-                .expect(format!("Falha ao spawnar processo {i}").as_str());
-            childs.push(c);
-        }
-        // Aguardar a finalização de todos os agentes
-        for mut c in childs {
-            c.wait().expect("Falha ao esperar processo filho");
-        }
-        calculate_test(agent_num);
-
-    } else if args.len() == 15 {
-        // Se há 13 argumentos, então está rodando um subprocesso
-        let n_msgs: u32 = args[2].parse().expect("Falha ao converter n_msgs para u32");
-        let ip: IpAddr = args[8]
-            .parse()
-            .expect("Falha ao converter ip para IpAddr");
-        let port: u16 = args[9]
-            .parse()
-            .expect("Falha ao converter port para u16");
-        let agent_id: usize = args.last().unwrap()
-            .parse()
-            .expect("Falha ao converter agent_id para u32");
-        
-        let agent = create_agents(
-            agent_id,
-            agent_num,
-            n_msgs,
-            ip,
-            port,
-        );
-        let actions = test.remove(agent_id);
-        match agent {
-            Ok(agent) => agent.run(actions),
-            Err(e) => panic!("Falha ao criar agente {}: {}", agent_id, e),
-        }
-    } else {
-        println!("uso: cargo run <agent_num> <n_msgs> <broadcast> <timeout> <timeout_limit> <message_timeout> <broadcast_timeout> <ip> <port> <gossip_rate> <w_size> <buffer_size> <loss_rate> <corruption_rate>");
-        println!("enviado {:?}", args);
-        panic!("Número de argumentos {} inválido", args.len());
-    }
 }
