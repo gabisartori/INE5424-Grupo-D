5a805fcfbe6f08dfa4bd77927f61305e052739d0
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2024-11-06 17:10:17 -0300

    Encapsualndo alteração do checksum

--- relcomm/src/types_packet.rs
+++ relcomm/src/types_packet.rs
@@ -1,5 +1,6 @@
 // Importações necessárias
 use std::net::SocketAddr;
+use crate::config::CORRUPTION_RATE;
 use crate::types_header::{HBrd, HSnd, HLRq, Ack, AckBrd, Header, DataHeader, IsAck};
 /// Estrutura básica para pacotes com uma mensagem
 #[derive(Clone)]
@@ -16,6 +17,155 @@ pub enum PacketType {
     LeaderRequest(DataPkt<HLRq>),
     Ack(Ack),
     AckBrd(AckBrd),
+}
+
+/// checksum and to_bytes interface, and get_data
+impl PacketType {
+    /// Para a conversão de bytes para pacotes
+    /// Também emula a corrupção de pacotes
+    pub fn from_bytes(bytes: Vec<u8>) -> Self {
+        let type_h = bytes[0];
+        let bytes = bytes[1..].to_vec();
+        let mut pkt = match type_h {
+            Ack::SD => {
+                PacketType::Send(DataPkt::from_bytes(bytes))
+            },
+            Ack::BD => {
+                PacketType::Broadcast(DataPkt::from_bytes(bytes))
+            },
+            Ack::ACK => {
+                PacketType::Ack(<Ack as Header>::from_bytes(bytes))
+            },
+            Ack::ACK_BD => {
+                PacketType::AckBrd(<AckBrd as Header>::from_bytes(bytes))
+            },
+            Ack::LREQ => {
+                PacketType::LeaderRequest(DataPkt::from_bytes(bytes))
+            },
+            _ => panic!("Invalid packet type"),
+        };
+
+        if rand::random::<f32>() < CORRUPTION_RATE {
+            pkt.set_checksum(1);
+        }
+        pkt
+    }
+
+    /// Confere o checksum do pacote com o cálculo certo
+    pub fn validate_message(&self) -> bool {
+        self.get_checksum() == Self::checksum(self)
+    }
+
+    /// Chama a função de checksum do pacote
+    fn checksum(pkt: &Self) -> u32 {
+        match pkt {
+            PacketType::Ack(pkt) => <Ack as Header>::checksum(pkt),
+            PacketType::Send(pkt) => DataPkt::checksum(pkt),
+            PacketType::Broadcast(pkt) => DataPkt::checksum(pkt),
+            PacketType::AckBrd(pkt) => <AckBrd as Header>::checksum(pkt),
+            PacketType::LeaderRequest(pkt) => DataPkt::checksum(pkt),
+        }
+    }
+
+    /// Altera o checksum do pacote, para simular corrupção
+    fn set_checksum(&mut self, value: u32) {
+        match self {
+            PacketType::Ack(pkt) => pkt.checksum += value,
+            PacketType::AckBrd(pkt) => pkt.checksum += value,
+            PacketType::Send(pkt) => pkt.header.checksum += value,
+            PacketType::Broadcast(pkt) => pkt.header.checksum += value,
+            PacketType::LeaderRequest(pkt) => pkt.header.checksum += value,
+        }
+    }
+}
+
+/// essencial para Distribuição de pacotes
+pub trait Packet {
+    type Header: Header;
+    fn checksum(pkt: &Self) -> u32;
+    fn from_bytes(bytes: Vec<u8>) -> Self;
+    fn to_bytes(&self) -> Vec<u8>;
+}
+
+impl<A> Packet for A where A: IsAck + Header {
+    type Header = A;
+    fn checksum(pkt: &Self) -> u32 {
+        A::checksum(pkt)
+    }
+
+    fn from_bytes(bytes: Vec<u8>) -> Self {
+        A::from_bytes(bytes)
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        A::to_bytes(&self)
+    }
+}
+
+impl<H> Packet for DataPkt<H> where H: DataHeader + Header {
+    type Header = H;
+    fn checksum(pkt: &Self) -> u32 {
+        let sum: u32 = pkt.data.iter().fold(0, |acc, &byte| acc.wrapping_add(byte as u32));
+        sum.wrapping_add(H::checksum(&pkt.header))
+    }
+
+    fn from_bytes(bytes: Vec<u8>) -> Self {
+        let data = bytes[H::size()..].to_vec();
+        let header = H::from_bytes(bytes);
+        DataPkt{header, data}
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = self.header.to_bytes();
+        bytes.extend_from_slice(&self.data);
+        bytes
+    }
+}
+
+// Para evitar boilerplate, implementa HasData para pacotes que contêm dados
+pub trait HasData<H: DataHeader>: Sized + Packet {
+    type AckType: IsAck + Get;
+    fn new(header: H, data: Vec<u8>) -> Self;
+    fn pkts_from_msg(addrs: Vec<&SocketAddr>, seq_num: u32, data: Vec<u8>) -> Vec<Self>;
+    fn plain_new(header: H, data: Vec<u8>) -> Self;
+    fn get_ack(&self) -> Self::AckType;
+}
+
+impl<H> HasData<H> for DataPkt<H>
+where
+    H: DataHeader,
+    DataPkt<H>: Get,
+    H::AckType: IsAck + Get,{
+    type AckType = H::AckType;
+
+    fn new(header: H, data: Vec<u8>) -> Self {
+        let mut pkt = DataPkt::<H>::plain_new(header, data);
+        pkt.header.set_checksum(Packet::checksum(&pkt));
+        pkt
+    }
+
+    fn pkts_from_msg(addrs: Vec<&SocketAddr>, seq_num: u32, data: Vec<u8>) -> Vec<Self> {
+        let chunks: Vec<&[u8]> = data.chunks(Self::BUFFER_SIZE - H::size()).collect();
+
+        chunks.iter().enumerate().map(|(i, chunk)| {
+            let header = H::new(&addrs, seq_num + i as u32, i == (chunks.len() - 1));
+            DataPkt::<H>::new(
+                header,
+                chunk.to_vec(),
+            )
+        }).collect()        
+    }
+
+    fn plain_new(header: H, data: Vec<u8>) -> Self {
+        DataPkt {
+            header,
+            data,
+        }
+    }
+
+    fn get_ack(&self) -> Self::AckType {
+        self.header.get_ack()
+    }
 }
 
 /// getters para os campos essenciais dos pacotes
@@ -219,180 +369,4 @@ impl Get for AckBrd {
     fn type_h(&self) -> &str {
         Self::TYPE
     }    
-}
-
-/// Para a conversão de bytes para pacotes
-pub trait FromBytes {
-    fn from_bytes(bytes: Vec<u8>) -> Self;
-}
-
-impl FromBytes for PacketType {
-    fn from_bytes(bytes: Vec<u8>) -> Self {
-        let type_h = bytes[0];
-        let bytes = bytes[1..].to_vec();
-        match type_h {
-            Ack::SD => {
-                PacketType::Send(DataPkt::from_bytes(bytes))
-            },
-            Ack::BD => {
-                PacketType::Broadcast(DataPkt::from_bytes(bytes))
-            },
-            Ack::ACK => {
-                PacketType::Ack(<Ack as Header>::from_bytes(bytes))
-            },
-            Ack::ACK_BD => {
-                PacketType::AckBrd(<AckBrd as Header>::from_bytes(bytes))
-            },
-            Ack::LREQ => {
-                PacketType::LeaderRequest(DataPkt::from_bytes(bytes))
-            },
-            _ => panic!("Invalid packet type"),
-        }
-    }
-}
-
-/// checksum and to_bytes interface, and get_data
-impl PacketType {
-    pub fn checksum(pkt: &Self) -> u32 {
-        match pkt {
-            PacketType::Ack(pkt) => <Ack as Header>::checksum(pkt),
-            PacketType::Send(pkt) => DataPkt::<HSnd>::checksum(pkt),
-            PacketType::Broadcast(pkt) => DataPkt::<HBrd>::checksum(pkt),
-            PacketType::AckBrd(pkt) => <AckBrd as Header>::checksum(pkt),
-            PacketType::LeaderRequest(pkt) => DataPkt::<HLRq>::checksum(pkt),
-        }
-    }
-}
-
-
-/// essencial para Distribuição de pacotes
-pub trait Packet {
-    type Header: Header;
-    fn checksum(pkt: &Self) -> u32;
-    fn from_bytes(bytes: Vec<u8>) -> Self;
-    fn to_bytes(&self) -> Vec<u8>;
-}
-
-impl<A> Packet for A where A: IsAck + Header {
-    type Header = A;
-    fn checksum(pkt: &Self) -> u32 {
-        A::checksum(pkt)
-    }
-
-    fn from_bytes(bytes: Vec<u8>) -> Self {
-        A::from_bytes(bytes)
-    }
-
-    fn to_bytes(&self) -> Vec<u8> {
-        A::to_bytes(&self)
-    }
-}
-
-impl<H> Packet for DataPkt<H> where H: DataHeader + Header {
-    type Header = H;
-    fn checksum(pkt: &Self) -> u32 {
-        let sum: u32 = pkt.data.iter().fold(0, |acc, &byte| acc.wrapping_add(byte as u32));
-        sum.wrapping_add(H::checksum(&pkt.header))
-    }
-
-    fn from_bytes(bytes: Vec<u8>) -> Self {
-        let data = bytes[H::size()..].to_vec();
-        let header = H::from_bytes(bytes);
-        DataPkt{header, data}
-    }
-
-    fn to_bytes(&self) -> Vec<u8> {
-        let mut bytes = self.header.to_bytes();
-        bytes.extend_from_slice(&self.data);
-        bytes
-    }
-}
-
-// Para evitar boilerplate, implementa HasData para pacotes que contêm dados
-pub trait HasData<H: DataHeader>: Sized + Packet {
-    type AckType: IsAck + Get;
-    fn new(header: H, data: Vec<u8>) -> Self;
-    fn pkts_from_msg(addrs: Vec<&SocketAddr>, seq_num: u32, data: Vec<u8>) -> Vec<Self>;
-    fn plain_new(header: H, data: Vec<u8>) -> Self;
-    fn get_ack(&self) -> Self::AckType;
-}
-
-impl<H> HasData<H> for DataPkt<H>
-where
-    H: DataHeader,
-    DataPkt<H>: Set + Get,
-    H::AckType: IsAck + Get,{
-    type AckType = H::AckType;
-
-    fn new(header: H, data: Vec<u8>) -> Self {
-        let mut pkt = DataPkt::<H>::plain_new(header, data);
-        pkt.set_checksum(Packet::checksum(&pkt));
-        pkt
-    }
-
-    fn pkts_from_msg(addrs: Vec<&SocketAddr>, seq_num: u32, data: Vec<u8>) -> Vec<Self> {
-        let chunks: Vec<&[u8]> = data.chunks(Self::BUFFER_SIZE - H::size()).collect();
-
-        chunks.iter().enumerate().map(|(i, chunk)| {
-            let header = H::new(&addrs, seq_num + i as u32, i == (chunks.len() - 1));
-            DataPkt::<H>::new(
-                header,
-                chunk.to_vec(),
-            )
-        }).collect()        
-    }
-
-    fn plain_new(header: H, data: Vec<u8>) -> Self {
-        DataPkt {
-            header,
-            data,
-        }
-    }
-
-    fn get_ack(&self) -> Self::AckType {
-        self.header.get_ack()
-    }
-}
-
-/// setter do checksum
-pub trait Set {
-    fn set_checksum(&mut self, value: u32);
-}
-impl Set for DataPkt<HSnd> {
-    fn set_checksum(&mut self, value: u32) {
-        self.header.checksum += value;
-    }
-}
-impl Set for DataPkt<HBrd> {
-    fn set_checksum(&mut self, value: u32) {
-        self.header.checksum += value;
-    }
-}
-impl Set for DataPkt<HLRq> {
-    fn set_checksum(&mut self, value: u32) {
-        self.header.checksum += value;
-    }
-}
-impl Set for Ack {
-    fn set_checksum(&mut self, value: u32) {
-        self.checksum += value;
-    }
-}
-
-impl Set for AckBrd {
-    fn set_checksum(&mut self, value: u32) {
-        self.checksum += value;
-    }
-}
-
-impl Set for PacketType {
-    fn set_checksum(&mut self, value: u32) {
-        match self {
-            PacketType::Ack(pkt) => pkt.set_checksum(value),
-            PacketType::AckBrd(pkt) => pkt.set_checksum(value),
-            PacketType::Send(pkt) => pkt.set_checksum(value),
-            PacketType::Broadcast(pkt) => pkt.set_checksum(value),
-            PacketType::LeaderRequest(pkt) => pkt.set_checksum(value),
-        }
-    }
 }
