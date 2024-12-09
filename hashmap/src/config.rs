554e5a7107528778371c972975d8bce53410047f
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-12-08 23:30:13 -0300

    Adjust which debugs will be used

--- hashmap/src/config.rs
+++ hashmap/src/config.rs
@@ -1,7 +1,7 @@
 pub const TESTS_NUM: usize = 1;
-pub const MSG_NUM: u32 = 100;
+pub const MSG_NUM: u32 = 1000;
 pub const AGENT_NUM: usize = 10;
-pub const MSG_SIZE: usize = 1<<20;
+pub const MSG_SIZE: usize = 1<<10;
 pub const KEYS: [&str; 10] = [
     "key1",
     "key2",
@@ -14,4 +14,4 @@ pub const KEYS: [&str; 10] = [
     "key9",
     "key10"
 ];
-pub const WRITE_READ_RATIO: f32 = 0.5;
+pub const WRITE_READ_RATIO: f32 = 1.;
